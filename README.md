0e993a08eaba0fef902a8a48c4f4c6752b322a33
Hibrahim G. Corona <hibrahim.corona@outlook.com>
2026-07-23 08:45:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# open-torbox-net
---
+++ src/AltairOps.Torbox.Api/Program.cs
@@ -0,0 +1,26 @@
+using AltairOps.Torbox.Models.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient<TorboxService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.MapOpenApi();
+app.MapScalarApiReference();
+//}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
