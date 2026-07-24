2606233ed025edd481bd9e1114e3b95f8635745c
Hibrahim G. Corona <hibrahim.corona@outlook.com>
2026-07-24 11:01:14 -0700

    Clean-up code and fixes/changes to the CheckCached endpoint

--- src/AltairOps.Torbox.Models/Requests/Torrents/TorrentCheckCachedRequest.cs
+++ src/AltairOps.Torbox.Models/Requests/Torrents/TorrentCheckCachedRequest.cs
@@ -1,14 +1,18 @@
-﻿using System.Text.Json.Serialization;
+﻿using AltairOps.Torbox.Models.Enums;
+using System.Text.Json.Serialization;
 
 namespace AltairOps.Torbox.Models.Requests.Torrents;
 
+/// <summary>
+/// Represents a request to check if a torrent is cached in TorBox.
+/// </summary>
 public class TorrentCheckCachedRequest
 {
 	[JsonPropertyName("hash")]
 	public string? Hash { get; set; }
 
 	[JsonPropertyName("format")]
-	public string? Format { get; set; }
+	public Format? Format { get; set; }
 
 	[JsonPropertyName("list_files")]
 	public bool ListFiles { get; set; } = false;
---
+++ src/AltairOps.Torbox.Models/Responses/Torrents/TorrentCheckCachedItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AltairOps.Torbox.Models.Responses.Torrents;
+
+/// <summary>
+/// Represents a cached torrent item in the response for checking if a torrent is cached in TorBox.
+/// </summary>
+public class TorrentCheckCachedItem
+{
+	[JsonPropertyName("name")]
+	public string? Name { get; set; }
+
+	[JsonPropertyName("size")]
+	public long? Size { get; set; }
+
+	[JsonPropertyName("hash")]
+	public string? Hash { get; set; }
+
+	[JsonPropertyName("files")]
+	public List<TorrentFile>? Files { get; set; }
+}
+
+/// <summary>
+/// Represents a response containing a list of cached torrent items in TorBox.
+/// </summary>
+public class TorrentCheckCachedListResponse : List<TorrentCheckCachedItem?>
+{
+
+}
+
+/// <summary>
+/// Represents a response containing a dictionary of cached torrent items in TorBox, where the key is the torrent hash and the value is the corresponding cached torrent item.
+/// </summary>
+public class TorrentCheckCachedObjectResponse : Dictionary<string, TorrentCheckCachedItem?>
+{
+
+}
